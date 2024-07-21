ae8a0b6ee818083b1d235a6e32f550986bed4e20
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 15:45:39 +0300

    InvertedLetterPattern

---
+++ algorithm_level_2/14_inverted_letter_pattern.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int  ReadPositiveNumber(string Message) {
+	int Num;
+	do {
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+	return  Num;
+}
+
+void InvertedLetterPattern(int Num) {
+	for (int i = Num + 65 - 1; i >= 65; i--)
+	{
+		for (int j = 0; j <= i - 65; j++)
+		{
+			cout << char(i);
+		}
+		cout << endl;
+	}
+}
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	int Num;
+	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
+	InvertedLetterPattern(Num);
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
