b36ebaec23351a5f83a61fd03f0becfd12abe51d
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-22 02:32:49 +0300

    CheckRepeatedNumInArray ReadArray PrintArray

--- algorithm_level_2/21_generate_keys.cpp
+++ algorithm_level_2/21_generate_keys.cpp
@@ -1,59 +1,58 @@
-#include <iostream>
-#include <string>
-using namespace std;
-enum enDigit { SmallLetter = 1, CapitalLetter = 2, Digit = 3, SpecialCharacter = 4, };
-int  ReadPositiveNumber(string Message) {
-	int Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return  Num;
-}
-int RandomNumber(int From, int To) {
-	return (rand() % (To - From)) + From;
-}
-
-int GetRandomSelection(enDigit Selection) {
-	switch (Selection)
-	{
-	case enDigit::SmallLetter:
-		return RandomNumber(97, 122);
-	case enDigit::CapitalLetter:
-		return RandomNumber(65, 90);
-	case enDigit::Digit:
-		return RandomNumber(48, 57);
-	case enDigit::SpecialCharacter:
-		return RandomNumber(33, 47);
-	default:
-		return RandomNumber(58, 64);
-	}
-}
-string GenerateKey(int Num) {
-	string Key = "";
-	for (int i = 1; i <= Num; i++)
-	{
-		int Char = GetRandomSelection(enDigit::CapitalLetter);
-		Key += char(Char);
-		if (i % 4 == 0 && i != Num)		Key += '-';
-	}
-	return Key;
-}
-void GenerateNumberOfKeys(int Num) {
-	for (int i = 0; i < Num; i++)
-	{
-		cout << "Key [" << i+1 << "]: \t" << GenerateKey(16) << endl;
-	}
-}
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	srand((unsigned)time(NULL));
-	GenerateNumberOfKeys(16);
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
-
-
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//enum enDigit { SmallLetter = 1, CapitalLetter = 2, Digit = 3, SpecialCharacter = 4, };
+//int  ReadPositiveNumber(string Message) {
+//	int Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return  Num;
+//}
+//int RandomNumber(int From, int To) {
+//	return (rand() % (To - From)) + From;
+//}
+//int GetRandomSelection(enDigit Selection) {
+//	switch (Selection)
+//	{
+//	case enDigit::SmallLetter:
+//		return RandomNumber(97, 122);
+//	case enDigit::CapitalLetter:
+//		return RandomNumber(65, 90);
+//	case enDigit::Digit:
+//		return RandomNumber(48, 57);
+//	case enDigit::SpecialCharacter:
+//		return RandomNumber(33, 47);
+//	default:
+//		return RandomNumber(58, 64);
+//	}
+//}
+//string GenerateKey(short Num) {
+//	string Key = "";
+//	for (short i = 1; i <= Num; i++)
+//	{
+//		short Char = GetRandomSelection(enDigit::CapitalLetter);
+//		Key += char(Char);
+//		if (i % 4 == 0 && i != Num)		Key += '-';
+//	}
+//	return Key;
+//}
+//void GenerateNumberOfKeys(short Num) {
+//	for (short i = 0; i < Num; i++)
+//	{
+//		cout << "Key [" << i+1 << "]: \t" << GenerateKey(16) << endl;
+//	}
+//}
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	srand((unsigned)time(NULL));
+//	GenerateNumberOfKeys(ReadPositiveNumber("ENTER NUMBER OF KEYS: "));
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
+//
+//
