e078eb0f7f0ef17a308dc7576546c0860de83a3d
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 16:21:43 +0300

    PrintAllLettersAAAtoZZZSoln2

--- algorithm_level_2/15_normal_letter_pattern.cpp
+++ algorithm_level_2/15_normal_letter_pattern.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int  ReadPositiveNumber(string Message) {
-	int Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return  Num;
-}
-
-void NormalPattern(int Num) {
-	for (int i = 0; i <= Num; i++)
-	{
-		for (int j = 0; j <= i; j++)
-		{
-			cout << char(i + 65);
-		}
-		cout << endl;
-	}
-
-
-
-}
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	int Num;
-	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
-	NormalPattern(Num);
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int  ReadPositiveNumber(string Message) {
+//	int Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return  Num;
+//}
+//
+//void NormalLetterPattern(int Num) {
+//	for (int i = 0; i <= Num; i++)
+//	{
+//		for (int j = 0; j <= i; j++)
+//		{
+//			cout << char(i + 65);
+//		}
+//		cout << endl;
+//	}
+//
+//
+//
+//}
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	int Num;
+//	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
+//	NormalLetterPattern(Num);
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
