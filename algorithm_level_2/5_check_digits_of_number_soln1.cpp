bc02327ee1d5fa242b8496aa614b360773a717c1
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 13:08:08 +0300

    5_check_digits_of_number_soln2

--- algorithm_level_2/5_check_digits_of_number_soln1.cpp
+++ algorithm_level_2/5_check_digits_of_number_soln1.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int  ReadPositiveNumber(string Message) {
-	int Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return  Num;
-}
-void PrintDigit(char Digit) {
-	cout << "Digit ===>>>> \t" << Digit << endl;
-}
-void CheckDigitsOfNumber(int Num) {
-	string strNum = to_string(Num);
-	int LengthOfNum = strNum.length();
-	for (int i = LengthOfNum - 1; i >= 0; i--) {
-		PrintDigit(strNum[i]);
-	}
-}
-
-
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	int Num;
-	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
-	CheckDigitsOfNumber(Num);
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int  ReadPositiveNumber(string Message) {
+//	int Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return  Num;
+//}
+//void PrintDigit(char Digit) {
+//	cout << "Digit ===>>>> \t" << Digit << endl;
+//}
+//void CheckDigitsOfNumber(int Num) {
+//	string strNum = to_string(Num);
+//	int LengthOfNum = strNum.length();
+//	for (int i = LengthOfNum - 1; i >= 0; i--) {
+//		PrintDigit(strNum[i]);
+//	}
+//}
+//
+//
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	int Num;
+//	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
+//	CheckDigitsOfNumber(Num);
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
---
+++ algorithm_level_2/5_check_digits_of_number_soln2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int  ReadPositiveNumber(string Message) {
+	int Num;
+	do {
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+	return  Num;
+}
+void PrintDigit(int Digit) {
+	cout << "Digit ===>>>> \t" << Digit << endl;
+}
+void CheckDigitsOfNumber(int Num) {
+	int Remainder = 0;
+	while (Num > 0) {
+		Remainder = Num % 10;
+		Num = Num / 10;
+		PrintDigit(Remainder);
+	}
+}
+
+
+
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	int Num;
+	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
+	CheckDigitsOfNumber(Num);
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
