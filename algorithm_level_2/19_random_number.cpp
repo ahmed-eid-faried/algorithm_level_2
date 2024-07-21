38bae55a8cef78476d5feb42e5386f3c28892e76
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-22 00:31:48 +0300

    RandomNumber

---
+++ algorithm_level_2/19_random_number.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int RandomNumber(int From, int To) {
+//	return (rand() % (To - From)) - From;
+//}
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	srand((unsigned)time(NULL));
+//	cout << RandomNumber(0, 100) << endl;
+//	cout << RandomNumber(0, 100) << endl;
+//	cout << RandomNumber(0, 100) << endl;
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
+//
+//
