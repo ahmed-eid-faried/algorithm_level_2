acd1cdff0102e78a859389e0279f15187f912566
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 16:44:17 +0300

    GuessPassword

--- algorithm_level_2/16_aaa_to_zzz.cpp
+++ algorithm_level_2/16_aaa_to_zzz.cpp
@@ -1,50 +1,50 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-void PrintAllLettersAAAtoZZZ() {
-	for (int i = 65; i <= 90; i++)
-	{
-		for (int j = 65; j <= 90; j++)
-		{
-			for (int k = 65; k <= 90; k++)
-			{
-				cout << char(i) << char(j) << char(k) << endl;
-			}
-			cout << "-----------------------------------------------" << endl;
-
-		}
-		cout << "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" << endl;
-
-	}
-}
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-
-	PrintAllLettersAAAtoZZZ();
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
-
-
-// Function to print all combinations from AAA to ZZZ
-void PrintAllLettersAAAtoZZZSoln2() {
-	for (int num = 0; num < 26 * 26 * 26; ++num) {
-		char first = 'A' + (num / (26 * 26)) % 26;
-		char second = 'A' + (num / 26) % 26;
-		char third = 'A' + num % 26;
-		cout << first << second << third << endl;
-
-		if (num % 26 == 25) {
-			cout << "-----------------------------------------------" << endl;
-		}
-
-		if (num % (26 * 26) == (26 * 26) - 1) {
-			cout << "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" << endl;
-		}
-	}
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//void PrintAllLettersAAAtoZZZ() {
+//	for (int i = 65; i <= 90; i++)
+//	{
+//		for (int j = 65; j <= 90; j++)
+//		{
+//			for (int k = 65; k <= 90; k++)
+//			{
+//				cout << char(i) << char(j) << char(k) << endl;
+//			}
+//			cout << "-----------------------------------------------" << endl;
+//
+//		}
+//		cout << "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" << endl;
+//
+//	}
+//}
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//
+//	PrintAllLettersAAAtoZZZ();
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
+//
+//
+//// Function to print all combinations from AAA to ZZZ
+//void PrintAllLettersAAAtoZZZSoln2() {
+//	for (int num = 0; num < 26 * 26 * 26; ++num) {
+//		char first = 'A' + (num / (26 * 26)) % 26;
+//		char second = 'A' + (num / 26) % 26;
+//		char third = 'A' + num % 26;
+//		cout << first << second << third << endl;
+//
+//		if (num % 26 == 25) {
+//			cout << "-----------------------------------------------" << endl;
+//		}
+//
+//		if (num % (26 * 26) == (26 * 26) - 1) {
+//			cout << "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" << endl;
+//		}
+//	}
+//}
---
+++ algorithm_level_2/17_guess_password.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string  ReadString(string Message) {
+	string Text;
+	cout << Message;
+	cin >> Text;
+	return  Text;
+}
+string GuessPassword(string CorrectPassword) {
+	string word = "";
+	int Trials = 0;
+	for (int i = 65; i <= 90; i++)
+	{
+		for (int j = 65; j <= 90; j++)
+		{
+			for (int k = 65; k <= 90; k++)
+			{
+				word += char(i);
+				word += char(j);
+				word += char(k);
+				Trials++;
+				cout << "Trial [" << Trials << "]: " << word << endl;
+
+				if ((CorrectPassword == word)) {
+					cout << "-----------------------------------------------" << endl;
+					cout << "-----------------------------------------------" << endl;
+					cout << "PASSWORD IS " << word << endl;
+					cout << "FOUND AFTER  " << Trials << " Trial(s)" << endl;
+					return word;
+				}
+				word = "";
+			}
+
+		}
+
+	}
+}
+
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+
+	GuessPassword(ReadString("ENDER CORRECT PASSWORD TO Guess IT: "));
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
+
+
