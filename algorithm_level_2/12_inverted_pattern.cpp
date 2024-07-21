266053e233778109b3a8aa6d776fb12388c02f32
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-21 15:38:52 +0300

    NormalPattern

--- algorithm_level_2/12_inverted_pattern.cpp
+++ algorithm_level_2/12_inverted_pattern.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int  ReadPositiveNumber(string Message) {
-	int Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return  Num;
-}
-void PrintRow(int Num) {
-	for (int i = 0; i < Num; i++)
-	{
-		cout << Num;
-	}
-	cout << endl;
-}
-void InvertedPattern(int Num) {
-	for (int i = Num; i > 0; i--)
-	{
-		//PrintRow(i);
-		for (int j = 0; j < i; j++)
-		{
-			cout << i;
-		}
-		cout << endl;
-	}
-}
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	int Num;
-	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
-	InvertedPattern(Num);
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int  ReadPositiveNumber(string Message) {
+//	int Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return  Num;
+//}
+//void PrintRow(int Num) {
+//	for (int i = 0; i < Num; i++)
+//	{
+//		cout << Num;
+//	}
+//	cout << endl;
+//}
+//void InvertedPattern(int Num) {
+//	for (int i = Num; i > 0; i--)
+//	{
+//		//PrintRow(i);
+//		for (int j = 0; j < i; j++)
+//		{
+//			cout << i;
+//		}
+//		cout << endl;
+//	}
+//}
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	int Num;
+//	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
+//	InvertedPattern(Num);
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
---
+++ algorithm_level_2/13_normal_pattern.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int  ReadPositiveNumber(string Message) {
+	int Num;
+	do {
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+	return  Num;
+}
+
+void NormalPattern(int Num) {
+	for (int i = 1; i <= Num; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << i;
+		}
+		cout << endl;
+	}
+}
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	int Num;
+	Num = ReadPositiveNumber("ENTER POSITIVE NUMBER: ");
+	NormalPattern(Num);
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
