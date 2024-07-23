af689445a09fb30326f79f0ad85d624bec888d9d
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-23 12:45:53 +0300

    MyRound

--- algorithm_level_2/48_my_ceil.cpp
+++ algorithm_level_2/48_my_ceil.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-float  ReadNumber(string Message) {
-	float Num;
-	cout << Message;
-	cin >> Num;
-	return  Num;
-}
-float GetFraction(float Num) {
-	return Num - (int)Num;
-}
-int MyCeil(float Num) {
-
-	int intgerPart = (int)Num;
-	if (abs(GetFraction(Num)) > 0 || Num == 0) {
-		if (Num > 0) {
-			return abs((int)Num) + 1;
-		}
-		else {
-			return  (int)Num;
-		}
-	}
-	else {
-		return Num;
-	}
-}
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	float Num = ReadNumber("ENTER Number: ");
-	cout << "============================================" << endl;
-	cout << "My Ceil Fun \t====>>>>> " << MyCeil(Num) << endl;
-	cout << "C++ Ceil Fun \t====>>>>> " << ceil(Num) << endl;
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//float  ReadNumber(string Message) {
+//	float Num;
+//	cout << Message;
+//	cin >> Num;
+//	return  Num;
+//}
+//float GetFraction(float Num) {
+//	return Num - (int)Num;
+//}
+//int MyCeil(float Num) {
+//
+//	int intgerPart = (int)Num;
+//	if (abs(GetFraction(Num)) > 0 || Num == 0) {
+//		if (Num > 0) {
+//			return abs((int)Num) + 1;
+//		}
+//		else {
+//			return  (int)Num;
+//		}
+//	}
+//	else {
+//		return Num;
+//	}
+//}
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	float Num = ReadNumber("ENTER Number: ");
+//	cout << "============================================" << endl;
+//	cout << "My Ceil Fun \t====>>>>> " << MyCeil(Num) << endl;
+//	cout << "C++ Ceil Fun \t====>>>>> " << ceil(Num) << endl;
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
---
+++ algorithm_level_2/49_round.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+float  ReadNumber(string Message) {
+	float Num;
+	cout << Message;
+	cin >> Num;
+	return  Num;
+}
+float GetFraction(float Num) {
+	return Num - (int)Num;
+}
+
+int MyCeilRound(float Num) {
+	int intgerPart = (int)Num;
+	if (Num > 0) {
+		return abs(intgerPart) + 1;
+	}
+	else {
+		return  intgerPart - 1;
+	}
+}
+int MyFloorRound(float Num) {
+	int intgerPart = (int)Num;
+	if (Num >= 0) {
+		return intgerPart;
+	}
+	else {
+		return  intgerPart;
+	}
+}
+int MyRound(float Num) {
+	int intgerPart = (int)Num;
+	if (abs(GetFraction(Num)) > 0) {
+		if (abs(GetFraction(Num)) >= 0.5) {
+			return MyCeilRound(Num);
+		}
+		else {
+			return MyFloorRound(Num);
+		}
+	}
+	else {
+		return Num;
+	}
+}
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	float Num = ReadNumber("ENTER Number: ");
+	cout << "============================================" << endl;
+	cout << "My Round Fun \t====>>>>> " << MyRound(Num) << endl;
+	cout << "C++ Round Fun \t====>>>>> " << round(Num) << endl;
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
