caf82d430cdf883e5e4139e666c59d59e1c8becf
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-23 12:11:40 +0300

    MyFloor

---
+++ algorithm_level_2/47_my_floor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+float  ReadNumber(string Message) {
+	float Num;
+	cout << Message;
+	cin >> Num;
+	return  Num;
+}
+float GetFraction(float Num) {
+	return Num - (int)Num;
+}
+int MyFloor(float Num) {
+
+	int intgerPart = (int)Num;
+	if (Num >= 0) {
+		return (int)Num;
+	}
+	else {
+		return (abs((int)Num) + 1) * -1;
+
+	}
+}
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	float Num = ReadNumber("ENTER Number: ");
+	cout << "============================================" << endl;
+	cout << "My Abs Fun \t====>>>>> " << MyFloor(Num) << endl;
+	cout << "C++ Abs Fun \t====>>>>> " << floor(Num) << endl;
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
