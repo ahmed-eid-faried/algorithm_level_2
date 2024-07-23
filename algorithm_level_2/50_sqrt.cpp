e537abd723a98435e02eaf266382237267ebccf2
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-23 13:03:17 +0300

    finish

--- algorithm_level_2/50_sqrt.cpp
+++ algorithm_level_2/50_sqrt.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-float  ReadNumber(string Message) {
-	float Num;
-	cout << Message;
-	cin >> Num;
-	return  Num;
-}
- 
-float MySqrt(float Num) {
-
-	return pow(Num, 0.5);
-}
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	float Num = ReadNumber("ENTER Number: ");
-	cout << "============================================" << endl;
-	cout << "My Floor Fun \t====>>>>> " << MySqrt(Num) << endl;
-	cout << "C++ Floor Fun \t====>>>>> " << sqrt(Num) << endl;
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//float  ReadNumber(string Message) {
+//	float Num;
+//	cout << Message;
+//	cin >> Num;
+//	return  Num;
+//}
+// 
+//float MySqrt(float Num) {
+//
+//	return pow(Num, 0.5);
+//}
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	float Num = ReadNumber("ENTER Number: ");
+//	cout << "============================================" << endl;
+//	cout << "My Floor Fun \t====>>>>> " << MySqrt(Num) << endl;
+//	cout << "C++ Floor Fun \t====>>>>> " << sqrt(Num) << endl;
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
