b82be30156d70a2462eaa1c2d1b16620dba10100
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-23 08:20:21 +0300

    CopyArrayByAddElement

--- algorithm_level_2/36_simi_dynamic_array.cpp
+++ algorithm_level_2/36_simi_dynamic_array.cpp
@@ -1,57 +1,57 @@
-#include <iostream>
-#include <string>
-using namespace std;
-enum enDigit { SmallLetter = 1, CapitalLetter = 2, Digit = 3, SpecialCharacter = 4, };
-
-int  ReadPositiveNumber(string Message) {
-	int Num;
-	do {
-		cout << Message;
-		cin >> Num;
-	} while (Num < 0);
-	return  Num;
-}
-int AddElementToSimiDynamicArray(short& ArrLength) {
-	ArrLength++;
-	return ReadPositiveNumber("Please enter a number? ");
-}
-void FillSimiDynamicArray(int Array[100], short& ArrLength) {
-	int IsFinish;
-	for (short i = 0; i < 100; i++)
-	{
-		Array[i] = AddElementToSimiDynamicArray(ArrLength);
-		cout << "Do you want to add more numbers? [0]:No,[1]Yes? ";
-		cin >> IsFinish;
-		if (IsFinish != 1)break;
-	}
-}
-void PrintArray(int Array[100], short Num) {
-	cout << "Original Array: \t";
-	if (Num > 100)Num = 100;
-	for (int i = 0; i < Num; i++)
-	{
-		cout << Array[i] << " ";
-
-	}
-	cout << endl;
-}
-
-
-int main() {
-	cout << "########################################################\n";
-	cout << "#####################-by-ahmed-mady-####################\n";
-	cout << "########################################################\n";
-	srand((unsigned)time(NULL));
-	int Array[100];
-	short ArrLength = 0;
-	FillSimiDynamicArray(Array, ArrLength);
-	cout << "============================================" << endl;
-	cout << "Array Length: " << ArrLength << endl;
-	PrintArray(Array, ArrLength);
-	cout << "============================================" << endl;
-	cout << "########################################################\n";
-	cout << "########################################################\n";
-	return 0;
-}
-
-
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//enum enDigit { SmallLetter = 1, CapitalLetter = 2, Digit = 3, SpecialCharacter = 4, };
+//
+//int  ReadPositiveNumber(string Message) {
+//	int Num;
+//	do {
+//		cout << Message;
+//		cin >> Num;
+//	} while (Num < 0);
+//	return  Num;
+//}
+//void AddElementToSimiDynamicArray(short  Num, int Array[100], short& ArrLength) {
+//	ArrLength++;
+//	Array[ArrLength - 1] = Num;
+//}
+//void FillSimiDynamicArray(int Array[100], short& ArrLength) {
+//	int IsFinish; //bool IsFinish to valid input
+//	for (short i = 0; i < 100; i++)
+//	{
+//		AddElementToSimiDynamicArray(ReadPositiveNumber("Please enter a number? "), Array, ArrLength);
+//		cout << "Do you want to add more numbers? [0]:No,[1]Yes? ";
+//		cin >> IsFinish;
+//		if (IsFinish != 1)break;
+//	}
+//}
+//void PrintArray(int Array[100], short Num) {
+//	cout << "Original Array: \t";
+//	if (Num > 100)Num = 100;
+//	for (int i = 0; i < Num; i++)
+//	{
+//		cout << Array[i] << " ";
+//
+//	}
+//	cout << endl;
+//}
+//
+//
+//int main() {
+//	cout << "########################################################\n";
+//	cout << "#####################-by-ahmed-mady-####################\n";
+//	cout << "########################################################\n";
+//	srand((unsigned)time(NULL));
+//	int Array[100];
+//	short ArrLength = 0;
+//	FillSimiDynamicArray(Array, ArrLength);
+//	cout << "============================================" << endl;
+//	cout << "Array Length: " << ArrLength << endl;
+//	PrintArray(Array, ArrLength);
+//	cout << "============================================" << endl;
+//	cout << "########################################################\n";
+//	cout << "########################################################\n";
+//	return 0;
+//}
+//
+//
---
+++ algorithm_level_2/37_copy_array_by_add_element_simi_dynamic.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+using namespace std;
+enum enDigit { SmallLetter = 1, CapitalLetter = 2, Digit = 3, SpecialCharacter = 4, };
+
+int  ReadPositiveNumber(string Message) {
+	int Num;
+	do {
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+	return  Num;
+}
+int RandomNumber(int From, int To) {
+	return (rand() % (To - From + 1)) + From;
+}
+void FillArrayByRandomNum(int Array[100], short  Num) {
+	for (short i = 0; i < Num; i++)
+	{
+		Array[i] = RandomNumber(0, 100);
+	}
+}
+void AddElementToSimiDynamicArray(short  Num, int Array[100], short& ArrLength) {
+	ArrLength++;
+	Array[ArrLength - 1] = Num;
+}
+//void FillSimiDynamicArray(int Array[100], short& ArrLength) {
+//	int IsFinish; //bool IsFinish to valid input
+//	for (short i = 0; i < 100; i++)
+//	{
+//		AddElementToSimiDynamicArray(ReadPositiveNumber("Please enter a number? "), Array, ArrLength);
+//		cout << "Do you want to add more numbers? [0]:No,[1]Yes? ";
+//		cin >> IsFinish;
+//		if (IsFinish != 1)break;
+//	}
+//}
+void PrintArray(int Array[100], short Num, string Message = "Original Array") {
+	cout << Message << ": \t";
+	if (Num > 100)Num = 100;
+	for (int i = 0; i < Num; i++)
+	{
+		cout << Array[i] << " ";
+
+	}
+	cout << endl;
+}
+void CopyArrayFun(int Array[100], short ArrLength, int CopyArray[100], short& CopyArrLength) {
+	for (short i = 0; i < ArrLength; i++)
+	{
+		AddElementToSimiDynamicArray(Array[i], CopyArray, CopyArrLength);
+	}
+}
+
+int main() {
+	cout << "########################################################\n";
+	cout << "#####################-by-ahmed-mady-####################\n";
+	cout << "########################################################\n";
+	srand((unsigned)time(NULL));
+	int Array[100];
+	int CopyArray[100];
+	short ArrLength = ReadPositiveNumber("ENTER LENGTH OF ARRAY: ");
+	short CopyArrLength = 0;
+	if (ArrLength > 100)ArrLength = 100;
+	cout << "============================================" << endl;
+	FillArrayByRandomNum(Array, ArrLength);
+	PrintArray(Array, ArrLength, "Original Array");
+	cout << "============================================" << endl;
+	CopyArrayFun(Array, ArrLength, CopyArray, CopyArrLength);
+	PrintArray(CopyArray, ArrLength, "Copy Array");
+	cout << "########################################################\n";
+	cout << "########################################################\n";
+	return 0;
+}
+
+
